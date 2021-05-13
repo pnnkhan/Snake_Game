0b4d10b7d83ff35b669123ea61436b0836940fd4
Kalle2705 <Kalle2705@users.noreply.github.com>
2021-05-13 22:07:51 +0700

    Add project files.

---
+++ demo_drawing/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace demo_drawing
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbDiem = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(431, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Điểm:";
+            // 
+            // lbDiem
+            // 
+            this.lbDiem.AutoSize = true;
+            this.lbDiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDiem.ForeColor = System.Drawing.Color.Green;
+            this.lbDiem.Location = new System.Drawing.Point(515, 188);
+            this.lbDiem.Name = "lbDiem";
+            this.lbDiem.Size = new System.Drawing.Size(20, 24);
+            this.lbDiem.TabIndex = 4;
+            this.lbDiem.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(429, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(115, 137);
+            this.panel1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(10, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Space: Play";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(10, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "F2: Pause";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "F1: NewGame";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(429, 224);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(115, 100);
+            this.panel2.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Green;
+            this.label9.Location = new System.Drawing.Point(92, 56);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 24);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "4";
+            this.label9.Click += new System.EventHandler(this.label9_Click_1);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Green;
+            this.label8.Location = new System.Drawing.Point(66, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 24);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "3";
+            this.label8.Click += new System.EventHandler(this.label8_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Green;
+            this.label7.Location = new System.Drawing.Point(36, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 24);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "2";
+            this.label7.Click += new System.EventHandler(this.label7_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Green;
+            this.label6.Location = new System.Drawing.Point(2, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 24);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "1";
+            this.label6.Click += new System.EventHandler(this.label6_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(31, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Level";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label10.Location = new System.Drawing.Point(438, 370);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 24);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Kunsubin";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(435, 331);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Click để chọn Level";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 403);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbDiem);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmDay_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmDay_KeyPress);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbDiem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ demo_drawing/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo_drawing
+{
+    public partial class frmMain : Form
+    {
+        //khởi tạo độ dài của rắn ban đầu
+        int doDai = 3;
+        // mảng các tọa độ của rắn
+        int[] x;
+        int[] y;
+        //khởi tạo ma trận giao diện
+        int[,] matrix = new int[20, 20];
+        //sử lý phím di chuyển
+        public static int GO_UP = 1;
+        public static int GO_DOWN = -1;
+        public static int GO_LEFT = 2;
+        public static int GO_RIGHT = -2;
+        bool status_vector = false;
+        //biến xét vị trí chạy mặc định khi vào game
+        int vector = GO_UP;
+        int diem = 0;
+        //trạng thái game
+        bool playGame = true;
+        public frmMain()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x = new int[400];
+            y = new int[400];
+            //khởi tạo 3 vi trí ban đầu của rắn
+            x[0] = 5;
+            y[0] = 5;
+
+            x[1] = 5;
+            y[1] = 6;
+
+            x[2] = 5;
+            y[2] = 7;
+            //
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                    matrix[i, j] = 0;
+            //khởi tạo vị trí mồi đầu tiên
+            matrix[10, 10] = 2;
+            //
+            label6.BackColor = Color.Yellow;
+            label7.BackColor = Color.White;
+            label8.BackColor = Color.White;
+            label9.BackColor = Color.White;
+            timer1.Interval = 400;
+
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //this.Paint =new PaintEventHandler(this.Form1_Paint);
+
+        }
+        //sự kiện vẽ khung chơi game, rắn và mồi ban đầu
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.Black);
+            SolidBrush sb = new SolidBrush(Color.White);
+            Graphics g = this.CreateGraphics();
+            //vẽ khung hình
+            g.DrawRectangle(p, 0, 0, 401, 401);
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                {
+                    //vẽ ma trận giao diện
+                    g.FillRectangle(sb, i * 20 + 1, j * 20 + 1, 18, 18);
+                    //vẽ mồi
+                    if (matrix[i, j] == 2)
+                        g.FillRectangle(new SolidBrush(Color.Black), i * 20 + 1, j * 20 + 1, 18, 18);
+
+                }
+            //vẽ rắn
+            for (int z = 1; z < doDai; z++)
+                g.FillRectangle(new SolidBrush(Color.Blue), x[z] * 20 + 1, y[z] * 20 + 1, 18, 18);
+            g.FillRectangle(new SolidBrush(Color.Green), x[0] * 20 + 1, y[0] * 20 + 1, 18, 18);
+            g.Dispose();
+            sb.Dispose();
+
+        }
+
+        private void frmDay_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        //đây là sự kiện bắt phím di chuyển phím chức năng của game
+        private void frmDay_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) {
+                if (vector != -GO_UP && status_vector) {
+                    vector = GO_UP;
+                    status_vector = false;
+                }
+            }
+            if (e.KeyCode == Keys.Down) {
+                if (vector != -GO_DOWN && status_vector) {
+                    vector = GO_DOWN;
+                    status_vector = false;
+                }
+            }
+            if (e.KeyCode == Keys.Left) {
+                if (vector != -GO_LEFT && status_vector) {
+                    vector = GO_LEFT;
+                    status_vector = false;
+                }
+            }
+            if (e.KeyCode == Keys.Right) {
+                if (vector != -GO_RIGHT && status_vector) {
+                    vector = GO_RIGHT;
+                    status_vector = false;
+                }
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                //playGame = true;
+                if (playGame)
+                {
+                    timer1.Enabled = true;
+                    playGame = false;
+                }
+            }
+            if (e.KeyCode == Keys.F1)
+            {
+                Application.Restart();
+            }
+            if (e.KeyCode == Keys.F2)
+            {
+                if (label3.Text == "F2: Pause")
+                {
+                    timer1.Enabled = false;
+                    label3.Text = "F2: Resume";
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                    label3.Text = "F2: Pause";
+                }
+            }
+        }
+        int X()
+        {
+            Random rd = new Random();
+            return rd.Next(0, 19);
+        }
+        int Y()
+        {
+            Random rd = new Random();
+            return rd.Next(0, 19);
+        }
+        bool testXY(int m, int n)
+        {
+            if (m == 0 && n == 0) return false;
+            for (int i = 0; i < doDai; i++)
+                if (m == x[i] && n == y[i]) return false;
+            return true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //khởi tạo 1 timer tuần hoàn chu trình chạy của chương trình
+            //dùng set thời gian timer để lập lever cho game
+            status_vector = true;
+            //dịch chuyển đuôi
+            for (int i = doDai - 1; i > 0; i--)
+            {
+                x[i] = x[i - 1];
+                y[i] = y[i - 1];
+            }
+            //dịch đầu
+            if (vector == GO_DOWN) y[0]++;
+            if (vector == GO_UP) y[0]--;
+            if (vector == GO_LEFT) x[0]--;
+            if (vector == GO_RIGHT) x[0]++;
+            //thiết lập khi đâm vào biên
+            if (x[0] < 0) x[0] = 19;
+            if (x[0] > 19) x[0] = 0;
+            if (y[0] < 0) y[0] = 19;
+            if (y[0] > 19) y[0] = 0;
+
+            SolidBrush sb = new SolidBrush(Color.White);
+            //Pen pen = new Pen(Color.Red);
+            Graphics g = this.CreateGraphics();
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                {
+                    g.FillRectangle(sb, i * 20 + 1, j * 20 + 1, 18, 18);
+                    if (matrix[i, j] == 2)
+                        g.FillRectangle(new SolidBrush(Color.Black), i * 20 + 1, j * 20 + 1, 18, 18);
+
+                }
+            //vẽ rắn
+            for (int z = 1; z < doDai; z++)
+                g.FillRectangle(new SolidBrush(Color.Blue), x[z] * 20 + 1, y[z] * 20 + 1, 18, 18);
+            g.FillRectangle(new SolidBrush(Color.Green), x[0] * 20 + 1, y[0] * 20 + 1, 18, 18);
+            //kiểm tra rắn ăn mồi chưa
+            if (matrix[x[0], y[0]] == 2)
+            {
+                doDai++;
+                diem += 10;
+                lbDiem.Text = diem.ToString();
+                g.FillRectangle(new SolidBrush(Color.White), 1, 1, 18, 18);
+                matrix[x[0], y[0]] = 0;
+                //vẽ lại rắn
+                for (int z = 1; z < doDai; z++)
+                    g.FillRectangle(new SolidBrush(Color.Blue), x[z] * 20 + 1, y[z] * 20 + 1, 18, 18);
+                g.FillRectangle(new SolidBrush(Color.Green), x[0] * 20 + 1, y[0] * 20 + 1, 18, 18);
+                int m, n;
+                do
+                {
+                    m = X();
+                    n = Y();
+                } while (testXY(m, n) == false);//kiểm tra random trên thân rắn không
+
+                // MessageBox.Show(m.ToString() + "   " + n.ToString());
+                matrix[m, n] = 2;
+                g.FillRectangle(new SolidBrush(Color.Black), m * 20 + 1, n * 20 + 1, 18, 18);
+            }
+            //xử lý đâm vào thân
+            for (int i = 1; i < doDai; i++)
+                if (x[0] == x[i] && y[0] == y[i])
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("Bạn đâm vào đuôi mình!\nĐiểm:  " + diem.ToString(), "Thông báo");
+                    Application.Restart();
+                    //DialogResult dg;
+                    //dg = MessageBox.Show("Bạn có muốn chơi lại không", "Thông báo", MessageBoxButtons.YesNo);
+                    //if (dg == DialogResult.Yes)
+                    //    Application.Restart();
+                    //else
+                    //{
+
+                    //}
+                }
+
+            g.Dispose();
+            sb.Dispose();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = false;
+            //DialogResult dg;
+            //dg = MessageBox.Show("Bạn có muốn chơi lại không", "Thông báo", MessageBoxButtons.YesNo);
+            //if (dg == DialogResult.Yes)
+            //{
+            //    Application.Exit();
+            //}
+            //else
+            //{
+            //    timer1.Enabled = true;
+            //}
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            //if (btnPause.Text == "Pause")
+            //{
+            //    timer1.Enabled = false;
+            //    btnPause.Text = "Resume";
+            //}
+            //else
+            //{
+            //    timer1.Enabled = true;
+            //    btnPause.Text = "Pause";
+            //}
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            //newGame();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //switch (comboBox1.Text)
+            //{
+            //    case "1":
+            //        timer1.Interval = 500;
+            //        break;
+            //    case "2":
+            //        timer1.Interval = 400;
+            //        break;
+            //    case "3":
+            //        timer1.Interval = 300;
+            //        break;
+            //    case "4":
+            //        timer1.Interval = 200;
+            //        break;
+            //    case "5":
+            //        timer1.Interval = 100;
+            //        break;
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //phần dưới là các sự kiện mình bắt để chọn lever cho game
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+
+            label6.BackColor = Color.Yellow;
+            label7.BackColor = Color.White;
+            label8.BackColor = Color.White;
+            label9.BackColor = Color.White;
+            timer1.Interval = 400;
+        }
+
+        private void label7_Click_1(object sender, EventArgs e)
+        {
+            label6.BackColor = Color.White;
+            label7.BackColor = Color.Yellow;
+            label8.BackColor = Color.White;
+            label9.BackColor = Color.White;
+            timer1.Interval = 300;
+        }
+
+        private void label8_Click_1(object sender, EventArgs e)
+        {
+
+            label6.BackColor = Color.White;
+            label7.BackColor = Color.White;
+            label8.BackColor = Color.Yellow;
+            label9.BackColor = Color.White;
+            timer1.Interval = 200;
+        }
+
+        private void label9_Click_1(object sender, EventArgs e)
+        {
+            label6.BackColor = Color.White;
+            label7.BackColor = Color.White;
+            label8.BackColor = Color.White;
+            label9.BackColor = Color.Yellow;
+            timer1.Interval = 100;
+        }
+    }
+}
